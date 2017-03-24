4e9b2bccbf21ee5aac72e9e82e0c4083d182312a
q305196499 <305196499@qq.com>
2017-03-24 11:31:34 +0800

    tuozhguai

--- demo.js
+++ demo.js
@@ -2,7 +2,17 @@ function ad(id){
 	var obj=document.getElementById(id)
 	var disX=0
 	var disY=0
-	obj.onmousedown=function(){
-		
+	obj.onmousedown=function(e){
+		disX=e.pageX-obj.offsetLeft
+		disY=e.pageY-obj.offsetTop
+		document.onmousemove=function(e){
+			obj.style.left=e.pageX-disX+"px"
+			obj.style.top=e.pageY-disY+"px"
+		}
+		document.onmouseup=function(){
+			document.onmousemove=null
+			document.onmouseup=null
+		}
 	}
+	return false
 }
